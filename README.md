f1df703e08ed3f216a7354d23dac05b693e3c231
excitom <tlang@halsoft.com>
2023-04-09 22:46:37 +0000

    Implement SSL with a multi-threaded instead of event-driver web server.

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ since I wrote a lot of operating system and network level code back around
 the turn of the century, but my career evolved into web development (PHP,
 javascript, CSS) and subsequently engineering management. I'm having fun with
 this project, revisiting old skills.
+
+Note: While implementing SSL I ran into trouble getting `SSL_accept`
+to work with `epoll` and asynchronous I/O, so I switched to a
+multi-threaded server implementation with synchronous I/O when 
+serving SSL. This is a work in progress.
--- sslServer.c
+++ sslServer.c
@@ -1,6 +1,11 @@
 /**
  * SSL Web Server
  *
+ * Note: My original design uses `epoll` and asynchronous socket I/O
+ * but I was having trouble getting that to work with SSL, so I 
+ * switch to a multi-threaded server for SSL. There is one thread
+ * launched per incoming request.
+ *
  * (c) Tom Lang 2/2023
  */
 
@@ -17,45 +22,78 @@
 #include <sys/epoll.h>
 #include <openssl/err.h>
 #include <openssl/ssl.h>
-#include <locale.h>
+#include <pthread.h>
 #include "server.h"
 
 // global variables
 struct globalVars g;
 
-char* buffer;
+void *processRequest( void *);
+
+struct _request {
+	int clientfd;
+	SSL_CTX *ctx;
+};
+
+struct _thread {
+	struct _thread *next;
+	pthread_t thread;
+};
 
 /**
- * initialize the SSL server
+ * The SSL server
  */
 void
 sslServer()
 {
-	buffer = malloc(BUFF_SIZE);
-
 	SSL_CTX *ctx;
-	SSL *ssl = NULL;
 	ctx = createContext();
 	configureContext(ctx);
 	int sockfd = createBindAndListen(g.port);
+	int threadCount = 0;
+	struct _thread *threadList = NULL;
 	while(1) {
 		struct sockaddr_in addr;
 		socklen_t len = sizeof(addr);
 		int clientfd = accept(sockfd, (struct sockaddr*)&addr, &len);
-		ssl = SSL_new(ctx);
-		SSL_set_fd(ssl, clientfd);
-		if ( SSL_accept(ssl) == FAIL ) {	 /* do SSL-protocol accept */
-			ERR_print_errors_fp(stderr);
-			perror("Accept failed");
-		} else {
-			processInput(clientfd, ssl);
-		}
-		shutdown(clientfd, SHUT_RDWR);
-		close(clientfd);
+		struct _request request;
+		request.clientfd = clientfd;
+		request.ctx = ctx;
+		pthread_t thread;
+		pthread_create(&thread, NULL, processRequest, (void *)&request);
+		threadCount++;
+		printf("Thread Count: %d\n", threadCount);
+		struct _thread *t = (struct _thread*)malloc(sizeof(struct _thread));
+		t->next = threadList;
+		threadList = t;
+		t->thread = thread;
 	}
 	close(sockfd);
 	SSL_CTX_free(ctx);
-	free(buffer);
+	while(threadList != NULL) {
+		pthread_join(threadList->thread, NULL);
+		threadList = threadList->next;
+	}
+}
+
+/**
+ * Thread for handling a request
+ */
+void *
+processRequest(void *param)
+{
+	struct _request *r = (struct _request *)param;
+	SSL *ssl = SSL_new(r->ctx);
+	SSL_set_fd(ssl, r->clientfd);
+	if ( SSL_accept(ssl) == FAIL ) {	 /* do SSL-protocol accept */
+		ERR_print_errors_fp(stderr);
+		perror("Accept failed");
+	} else {
+		processInput(r->clientfd, ssl);
+	}
+	shutdown(r->clientfd, SHUT_RDWR);
+	close(r->clientfd);
+	printf("Thread exiting\n");
 }
 
 /**
@@ -67,7 +105,7 @@ createContext()
 	const SSL_METHOD *method;
 	SSL_CTX *ctx;
 	method = TLS_server_method();
-OpenSSL_add_all_algorithms();  /* load & register all cryptos, etc. */
+	OpenSSL_add_all_algorithms();  /* load & register all cryptos, etc. */
 	SSL_load_error_strings();   /* load all error messages */
 
 	ctx = SSL_CTX_new(method);
