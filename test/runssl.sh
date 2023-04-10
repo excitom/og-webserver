d7bf689904d520fbdda0bac2465254b7dff8b4e5
excitom <tlang@halsoft.com>
2023-04-10 20:15:18 +0000

    add ssl tests

---
+++ test/runssl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+iterations=$1
+processes=$2
+echo "Iterations $iterations Processes $processes"
+while [ $processes -gt 0 ]
+do
+	echo $processes
+	./poundssl.pl $iterations 2>/dev/null && echo "DONE $processes" &
+	processes=$(( $processes - 1 ))
+done
