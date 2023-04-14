7e5bfd8a65fc844b5f16a1fa850b134bf4f74eb2
excitom <tlang@halsoft.com>
2023-04-14 16:30:31 +0000

    change default port for the ssl test

--- test/poundssl.pl
+++ test/poundssl.pl
@@ -4,7 +4,7 @@ $iterations = $ARGV[0] if ($#ARGV == 0);
 open my $handle, '<', "./list";
 chomp(my @lines = <$handle>);
 close $handle;
-$base = "https://www.halsoft.com:8080/";
+$base = "https://www.halsoft.com:4433/";
 while($iterations-- > 0) {
 	$i = rand @lines;
 	$url = $base . $lines[$i];
