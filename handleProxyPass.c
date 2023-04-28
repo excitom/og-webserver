ef22fe94b5482afd70cfc40a2743f8eb1d7e0ec0
excitom <tlang@halsoft.com>
2023-04-28 00:38:12 +0000

    add `X-Forwarded-For` header to `proxy_pass` handling

--- handleProxyPass.c
+++ handleProxyPass.c
@@ -18,24 +18,35 @@
 #include "global.h"
 
 void
-handleProxyPass(int fd, char *headers, char *path, _location *loc)
+handleProxyPass(int fd, char *headers, _location *loc)
 {
-	if (g.debug) {
-		fprintf(stderr, "PROXY_PASS to: %s\n", path);
-	}
-	int upstream = socket(AF_INET, SOCK_STREAM, 0);
-	if (upstream < 0) {
+    struct sockaddr_in server;
+    server.sin_family      = loc->passTo->sin_family;
+    server.sin_port        = loc->passTo->sin_port;
+    server.sin_addr.s_addr = loc->passTo->sin_addr.s_addr;
+	int upstream;
+    if ((upstream = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
 		doDebug("upstream socket failed.");
 		doDebug(strerror(errno));
 		return;
-	}
-	if (connect(upstream, (struct sockaddr *)loc->passTo, sizeof(loc->passTo)) < 0) {
+    }
+    if (connect(upstream, (struct sockaddr *)&server, sizeof(server)) < 0) {
 		doDebug("upstream connect failed.");
 		doDebug(strerror(errno));
 		return;
-	}
+    }
+	// change trailing \r\n\r\n to \r\n in order to
+	// add the `X-Forwarded-For` header
+	char *r = strrchr(headers, '\r');
+	*r = '\0';
 	sendData(upstream, NULL, headers, strlen(headers));
 	char buffer[BUFF_SIZE];
+	strcpy((char *)&buffer, "X-Forwarded-For: ");
+	_clientConnection *c = getClient(fd);
+	strcat((char *)&buffer, c->ip);
+	strcat((char *)&buffer, "\r\n\r\n");
+	sendData(upstream, NULL, buffer, strlen(buffer));
 	size_t bytes = BUFF_SIZE;
 	do {
 		bytes = recvData(upstream, (char *)&buffer, bytes);
