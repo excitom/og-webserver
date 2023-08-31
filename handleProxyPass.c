caea0a2fc6e07097e0fa3c036d6a9fbf1e48ccd7
excitom <tlang@halsoft.com>
2023-08-31 00:02:46 +0000

    implement load balancing with `upstream` directive

--- handleProxyPass.c
+++ handleProxyPass.c
@@ -28,32 +28,34 @@ getUpstreamServer(_request *req)
 			doDebug("Missing upstream group");
 			return -1;
 		}
-    	server.sin_family      = up->currentServer->passTo->sin_family;
-    	server.sin_port        = up->currentServer->passTo->sin_port;
-    	server.sin_addr.s_addr = up->currentServer->passTo->sin_addr.s_addr;
+		server.sin_family      = up->currentServer->passTo->sin_family;
+		server.sin_port        = up->currentServer->passTo->sin_port;
+		server.sin_addr.s_addr = up->currentServer->passTo->sin_addr.s_addr;
+		// prepare to handle the next request with the next server
 		up->currentServer = up->currentServer->next;
 		if (!up->currentServer) {
 			up->currentServer = up->servers;	// wrap at end of list
 		}
 
 	} else {
-    	server.sin_family      = req->loc->passTo->sin_family;
-    	server.sin_port        = req->loc->passTo->sin_port;
-    	server.sin_addr.s_addr = req->loc->passTo->sin_addr.s_addr;
+		// upstream is a single server
+		server.sin_family      = req->loc->passTo->sin_family;
+		server.sin_port        = req->loc->passTo->sin_port;
+		server.sin_addr.s_addr = req->loc->passTo->sin_addr.s_addr;
 	}
 	int upstream;
-    if ((upstream = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
+	if ((upstream = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
 		doDebug("upstream socket failed.");
 		doDebug(strerror(errno));
-		return -1;
-    }
-    if (connect(upstream, (struct sockaddr *)&server, sizeof(server)) < 0) {
+	return -1;
+	}
+	if (connect(upstream, (struct sockaddr *)&server, sizeof(server)) < 0) {
 		doDebug("upstream connect failed.");
 		doDebug(strerror(errno));
 		close(upstream);
 		return -1;
-    }
+	}
 	return upstream;
 }
 
